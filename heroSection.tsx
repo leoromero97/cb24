475d5d5a3e11fc68834ef7111e60f411634aa319
Leo G. Romero <leonardo.g.romero.dev@gmail.com>
2024-03-17 18:22:56 -0300

    Create heroSection.tsx

---
+++ heroSection.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="flex flex-col items-center text-center max-w-screen-lg py-20">
+      <h1 className="text-8xl font-bold">¡Felíz cumple mi amor! 🥳</h1>
+    </section>
+  );
+}
