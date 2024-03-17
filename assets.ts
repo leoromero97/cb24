ff1cf5eca6cb2a106f6a43123117e19a8bc21ad4
Leo G. Romero <leonardo.g.romero.dev@gmail.com>
2024-03-17 18:24:24 -0300

    Create assets.ts

---
+++ assets.ts
@@ -0,0 +1,33 @@
+import memoryImg from "@/app/assets/test1.jpg";
+import virtuesImg from "@/app/assets/test2.jpg";
+import gratitudeImg from "@/app/assets/test3.jpg";
+import gift1Img from "@/app/assets/gift1.jpg";
+import gift2Img from "@/app/assets/gift2.jpg";
+import gift3Img from "@/app/assets/gift3.jpg";
+
+export const ASSETS = {
+  memory: {
+    src: memoryImg,
+    alt: "Beso en beron de astrada",
+  },
+  virtues: {
+    src: virtuesImg,
+    alt: "Editar",
+  },
+  gratitude: {
+    src: gratitudeImg,
+    alt: "Editar",
+  },
+  gift1: {
+    src: gift1Img,
+    alt: "Regalo numero 1",
+  },
+  gift2: {
+    src: gift2Img,
+    alt: "Regalo numero 2",
+  },
+  gift3: {
+    src: gift3Img,
+    alt: "Regalo numero 3",
+  },
+};
